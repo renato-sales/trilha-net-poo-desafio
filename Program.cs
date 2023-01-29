497230cec16861f80e8e5719436fcd5721d2b24e
Renato Sales <renatosalespaula@gmail.com>
2023-01-28 21:22:42 -0300

    atualizando o código do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace Desafio
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia:");
+            Smartphone nokia = new Nokia(numero: "123456",modelo: "Modelo 1",imei: "111111111",memoria: 64);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Whatsapp");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Smartphone Iphone:");
+            Smartphone iphone = new Iphone(numero: "4987",modelo: "Modelo 2",imei: "2222222222",memoria: 128);
+            iphone.Ligar();
+            iphone.InstalarAplicativo("Telegram");
+        }
+    }
+}
